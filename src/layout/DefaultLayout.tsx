fe231d938e4229298b5bb5189044a57e55a99530
Roman Basharin <hellekrb@yandex.ru>
2023-11-19 13:20:03 +0300

    New dark theme

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,14 +1,18 @@
 import { ReactElement } from 'react'
 
+import { BlobsWrapper } from '@components/BlobsWrapper'
+
 import Header from './Header'
 
 const DefaultLayout = ({ children }: { children: ReactElement }) => (
-  <div className="grow bg-gray-200 text-gray-800 flex justify-center">
-    <div className="w-full max-w-7xl p-6">
-      <Header />
-      {children}
+  <BlobsWrapper className="grow">
+    <div className="flex justify-center">
+      <div className="w-full max-w-7xl p-6">
+        <Header />
+        {children}
+      </div>
     </div>
-  </div>
+  </BlobsWrapper>
 )
 
 export default DefaultLayout
